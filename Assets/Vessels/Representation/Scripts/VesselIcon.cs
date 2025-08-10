51559fb785d0133afd20d0023e122f342320fab4
someone-s <workemaileden@gmail.com>
2025-08-10 12:50:30 +0100

    fix icon bug

--- Assets/Vessels/Representation/Scripts/VesselIcon.cs
+++ Assets/Vessels/Representation/Scripts/VesselIcon.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 
@@ -10,10 +11,12 @@ public class VesselIcon : MonoBehaviour
     {
         string newText = string.Empty;
         var iter = cargos.GetEnumerator();
+        iter.MoveNext();
         for (int i = 0; i < target; i++)
         {
             if (i < cargos.Count)
             {
+                Debug.Log(iter.Current);
                 newText += SpriteCargo.Instance.Texts[iter.Current];
                 iter.MoveNext();
             }
